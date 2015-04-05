3d6cff8b3ca1ba4610ac69e54635fd0b8e79c3b6
Amir Mikhak <amirmikhak@notidle.com>
2015-04-04 20:17:14 -0400

    add shift to X, Y axes with ability to toggle wrapping

--- js/cube.js
+++ js/cube.js
@@ -100,18 +100,61 @@ var Cube = function(size, parentElement, playButton, clearButton, cellOptions) {
     return this;
 };
 
-Cube.prototype.shiftPlane = function(direction) {
+Cube.prototype.shiftPlane = function(axis, stepSize, wrap) {
+    stepSize = typeof stepSize !== 'undefined' ? stepSize : -1;
+    wrap = typeof wrap !== 'undefined' ? !!wrap : true;
+
     var me = this;
 
     var nextState = me.cells.map(function(cell) {
+        function getNewValueForShift(cell, axis) {
+            if ((cell[axis] + stepSize) >= 0 && (cell[axis] + stepSize) < me.size)
+            {   // your new coord originated from inside of bounds
+                return (cell[axis] + stepSize) % me.size;
+            } else
+            {   // your new coord originated from outside of bounds
+                if (wrap)
+                {   // reach around the other side
+                    return (me.size + cell[axis] + stepSize) % me.size;
+                } else
+                {   // screw it, your new value is nothing
+                    return -1;
+                }
+            }
+        };
+
+        function getNewRowForXShift(cell) {
+            return getNewValueForShift(cell, 'row');
+        }
+
+        function getNewColForYShift(cell) {
+            return getNewValueForShift(cell, 'column');
+        }
+
+        function getNewDepthForZShift(cell) {
+            return getNewValueForShift(cell, 'depth');
+        }
+
         // We want to calculate the coordinates of the 'previous' cell along various directions
         var shiftedCoords = {
-            'X': [], // Not yet implemented
-            'Y': [], // Not yet implemented
-            'Z': [cell.row, cell.column, (cell.depth - 1) >= 0 ? (cell.depth - 1) % me.size : (me.size + cell.depth - 1) % me.size]
-        }[direction];
+            'X': [
+                getNewRowForXShift(cell),
+                cell.column,
+                cell.depth,
+            ],
+            'Y': [
+                cell.row,
+                getNewColForYShift(cell),
+                cell.depth,
+            ],
+            'Z': [
+                cell.row,
+                cell.column,
+                getNewDepthForZShift(cell),
+            ]
+        }[axis];
 
-        // Once we have it, grab its on status and coolor and return it
+        // Once we have it, grab its on status and color and return it
         return {
             'on': me.getCellAt(shiftedCoords[0], shiftedCoords[1], shiftedCoords[2]).on,
             'color': me.getCellAt(shiftedCoords[0], shiftedCoords[1], shiftedCoords[2]).color
@@ -129,6 +172,21 @@ Cube.prototype.shiftPlane = function(direction) {
 };
 
 Cube.prototype.getCellAt = function(row, column, depth) {
+    if ((row < 0) || (row > this.size - 1) ||
+        (column < 0) || (column > this.size - 1) ||
+        (depth < 0) ||  (depth > this.size - 1))
+    {
+        /*
+         * @amirmikhak
+         * if impossible coordinate, return an object that _looks like_ an off cell.
+         * this is likely very confusing behavior, but it works for what I need in
+         * shiftPlane().
+         */
+        return {
+            on: false,
+            color: [0, 0, 0],
+        }
+    }
     return this.cells[depth * this.size * this.size + row * this.size + column];
 };
 
