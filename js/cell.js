amirmikhak/3D-GIF

commit c0fd5eb8f48a04ba291f077dafa675396d4cf7b0

clean up out-of-bounds checking of dimensions
- also remove redundancy in Cube.setCellAt()

js/cube.js
@@ -1709,6 +1709,18 @@ Cube.prototype.shiftPlane = function(axis, stepSize, wrap) {
     return this;    // enables multiple calls on cube to be "chained"
 };
 
+Cell.prototype.dimensionOutOfBounds = function(dimValue) {
+    return (vimValue < 0) || (vimValue >= this.size);
+}
+
+Cell.prototype.invalidCoord = function(r, c, d) {
+    return (
+        this.dimensionOutOfBounds(r) ||
+        this.dimensionOutOfBounds(c) ||
+        this.dimensionOutOfBounds(d)
+    );
+};
+
 Cube.prototype.getCellAt = function(row, column, depth) {
     /**
      * Returns the cell for a given coordinate. If the coordinate is invalid,
@@ -1719,9 +1731,7 @@ Cube.prototype.getCellAt = function(row, column, depth) {
      * properties that are copied between cells.
      */
 
-    if ((row < 0) || (row > this.size - 1) ||
-        (column < 0) || (column > this.size - 1) ||
-        (depth < 0) ||  (depth > this.size - 1))
+    if (this.invalidCoord(row, column, depth))
     {
         return new Cell({
             on: false,
@@ -1740,18 +1750,13 @@ Cube.prototype.setCellAt = function(row, column, depth, newCell) {
      * Throws "Invalid coordinate" if the coordinate is impossible.
      */
 
-    if ((row < 0) || (row > this.size - 1) ||
-        (column < 0) || (column > this.size - 1) ||
-        (depth < 0) ||  (depth > this.size - 1))
+    if (this.invalidCoord(row, column, depth))
     {
         console.error('Invalid Coord', row, column, depth, newCell);
         throw 'Invalid coordinate';
     }
 
-    var cellIndex = (depth * this.size * this.size) + (row * this.size) + column;
-    var matchedCell = this.cells[cellIndex];
-
-    matchedCell.setFromCell(newCell);
+    this.getCellAt(row, column, depth).setFromCell(newCell);
 
     return matchedCell;
 };
@@ -2346,12 +2351,12 @@ Cube.prototype.affectCol = function(dims, dim1, dim2, cb) {
     {
         console.error('affectCol(): Bad dimensions. Valid dimensions: ' + validDims.join(', '), dims);
         return;
-    } else if ((dim1 < 0) || (dim1 >= this.size))
+    } else if (this.dimensionOutOfBounds(dim1))
     {
         var dimName = dims.charAt(0).toUpperCase();
         console.error('affectCol(): Bad ' + dimName, dim1);
         return;
-    } else if ((dim2 < 0) || (dim2 >= this.size))
+    } else if (this.dimensionOutOfBounds(dim2))
     {
         var dimName = dims.charAt(1).toUpperCase();
         console.error('affectCol(): Bad ' + dimName, dim2);

commit a7cd277e2bb2da10d5586026dbe7486a4dd248aa

fix bugs introduced in c0fd5eb, 4e1cd05

js/cell.js
@@ -532,6 +532,8 @@ var Cell = function(opts) {
         }
         render();   // manually render all changes
         __autoRender = true;    // re-enable auto-rendering
+
+        return this;
     };
 
     (function buildHTML() {
@@ -557,7 +559,7 @@ Cell.prototype.setFromCell = function(otherCell) {
     /**
      * Copy visual properties from another cell into self.
      */
-    this.applyOptions({
+    return this.applyOptions({
         color: otherCell.color,
         on: otherCell.on,
     });

js/cube.js
@@ -1714,8 +1714,7 @@ Cube.prototype.shiftPlane = function(axis, stepSize, wrap) {
     return this;    // enables multiple calls on cube to be "chained"
 };
 
-Cell.prototype.dimensionOutOfBounds = function(dimValue) {
-    return (vimValue < 0) || (vimValue >= this.size);
+Cube.prototype.dimensionOutOfBounds = function(dimValue) {
 }
 
 Cell.prototype.invalidCoord = function(r, c, d) {

js/lib/misc-helpers.js
@@ -1,7 +1,11 @@
+
 function fetchJSONFile(path, successCb, failureCb) {
     /**
      * Helper function to make AJAX loading nicer. Grabbed from here: http://stackoverflow.com/questions/14388452/how-do-i-load-a-json-object-from-a-file-with-ajax
      */
+
+    var NOOP = function() {};
+
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if (httpRequest.readyState === 4)
@@ -20,9 +24,11 @@ function fetchJSONFile(path, successCb, failureCb) {
     };
     httpRequest.open('GET', path);
     httpRequest.send();
+
 }
 
 function tryJSON(data, validator) {
+
     var retData = data;
     try
     {   // handle different types of data input: JSON or raw object
@@ -37,4 +43,5 @@ function tryJSON(data, validator) {
     }
 
     return retData;
+
 }