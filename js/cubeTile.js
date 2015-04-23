amirmikhak/3D-GIF

commit 39cd2adcbf8ddef225f1db530297b18e09b2ed6d

tile.js -> cubeTile.js

js/cubeTile.js
@@ -1,0 +1,128 @@
+var CubeTile = function CubeTile(cells, cellOpts) {
+    var _cellOpts = cellOpts || {};
+
+    var __sliceValidator = function(dataToValidate) {
+        return (dataToValidate instanceof Array) && (dataToValidate.length === 64);
+    };
+
+    var __copyCell = function(originalCell) {
+        var mergedCellOptions = _.extend({
+            row: originalCell.row,
+            column: originalCell.column,
+            depth: originalCell.depth,
+            on: originalCell.on,
+            color: originalCell.color,
+        }, _cellOpts);
+        return new Cell(mergedCellOptions);
+    };
+
+    var _cells = tryJSON(cells, __sliceValidator).map(__copyCell);
+
+    Object.defineProperty(this, 'cells', {
+        get: function() { return _cells.slice(); },
+    });
+
+    return this;
+};
+
+CubeTile.prototype.getAsStrips = function() {
+    var strips = [[],[],[],[],[],[],[],[]];
+    for (var i = 0; i < 8; i++)
+    {
+        for (var j = 0; j < 8; j++)
+        {
+            strips[i][j] = _cells[Math.floor(i * 8) + j];
+        }
+    }
+    return strips;
+};
+
+CubeTile.prototype.setFromStrips = function(strips) {
+    var __stripIsInvalid = function(strip) {
+        return !(strip instanceof Array) || (strip.length !== 8);
+    };
+
+    if (!(strips instanceof Array) ||
+        (strips.length !== 8) ||
+        strips.some(__stripIsInvalid))
+    {
+        console.error('Invalid strips provided. Cannot compose.');
+        return;
+    }
+
+    for (var i = 0; i < 8; i++)
+    {
+        for (var j = 0; j < 8; j++)
+        {
+            _cells[Math.floor(i * 8) + j] = strips[i][j];
+        }
+    }
+
+    return this;
+};
+
+CubeTile.prototype.reflectX = function() {
+    return this.setFromStrips(this.getAsStrips().reverse());
+};
+
+CubeTile.prototype.reflectY = function() {
+    var _reflectedCells = [];
+
+    for (var col = 0; col < 8; col++)
+    {
+        var reflectedRow = [];
+        for (var row = 0; row < 8; row++)
+        {
+            reflectedRow.unshift(_cells[(col * 8) + row]);
+        }
+
+        _reflectedCells = _reflectedCells.concat(reflectedRow);
+    }
+
+    _cells = _reflectedCells;
+};
+
+CubeTile.prototype.getPngData = function() {
+    var PNG_OUTPUT_WIDTH = 64;
+    var PNG_OUTPUT_HEIGHT = 64;
+
+    var PIXEL_MULTIPLIER_W = Math.floor(PNG_OUTPUT_WIDTH / 8);
+    var PIXEL_MULTIPLIER_H = Math.floor(PNG_OUTPUT_HEIGHT / 8);
+
+    var c = document.createElement('canvas');
+    c.width = PNG_OUTPUT_WIDTH;
+    c.height = PNG_OUTPUT_HEIGHT;
+
+    var ctx = c.getContext('2d');
+
+    var id = ctx.createImageData(1, 1);
+    var d = id.data;
+
+    for (var idx = 0, numCells = _cells.length; idx < numCells; idx++)
+    {
+        var cell = _cells[idx];
+        cell.row = !isNaN(parseInt(cell.row, 10)) ? cell.row : Math.floor(idx % 8);
+        cell.column = !isNaN(parseInt(cell.column, 10)) ? cell.column : Math.floor(idx / 8);
+
+        var pixelOffsetX = cell.row * PIXEL_MULTIPLIER_W;
+        var pixelOffsetY = cell.column * PIXEL_MULTIPLIER_H;
+
+        for (var subpixelCol = 0; subpixelCol < PIXEL_MULTIPLIER_W; subpixelCol++)
+        {
+            for (var subpixelRow = 0; subpixelRow < PIXEL_MULTIPLIER_H; subpixelRow++)
+            {
+                d[0] = cell.color[0];
+                d[1] = cell.color[1];
+                d[2] = cell.color[2];
+                d[3] = cell.on ? 255 : 0;
+
+                var y = pixelOffsetX + subpixelRow; // the x/y are swapped in the slice serialization
+                var x = pixelOffsetY + subpixelCol;
+
+                ctx.putImageData(id, x, y);
+            }
+        }
+    }
+
+    return c.toDataURL();
+};

js/cube.js
@@ -433,9 +433,9 @@ Cube.prototype.writeSlice = function(data, face, offset) {
         face :
         'front';
 
-    var dataToUse = tryJSON(data, this.sliceValidator.bind(this));
-
-    var dataTile = new Tile(dataToUse);
+    var dataTile = (data instanceof CubeTile) ?
+        data :
+        new CubeTile(tryJSON(data, this.sliceValidator.bind(this)));
 
     var facesToReflectX = ['back', 'right'];
     var facesToReflectY = ['bottom'];
@@ -450,10 +450,10 @@ Cube.prototype.writeSlice = function(data, face, offset) {
         dataTile.reflectY();
     }
 
-    var cells = dataTile.getCells();
+    var cellsToWrite = dataTile.cells;
 
     function writeCellFromData(r, c, d) {
-        var cell = cells.shift();
+        var cell = cellsToWrite.shift();
         this.setCellAt(r, c, d, cell);
     };
 

js/emptyCubeTile.js
@@ -1,4 +1,4 @@
-var EmptyTile = function EmptyTile() {
+var EmptyCubeTile = function EmptyCubeTile() {
     var emptyCells = [];
     for (var i = 0; i < 64; i++)
     {
@@ -7,5 +7,5 @@ var EmptyTile = function EmptyTile() {
             color: [0,0,0],
         }));
     }
-    return new Tile(emptyCells);
+    return new CubeTile(emptyCells);
 };

js/playlist.js
@@ -29,7 +29,7 @@ var Playlist = function(opts) {
     var __tileThumbs = [];
     var __tileHtmls = [];
 
-    var __emptyTile = new EmptyTile();
+    var __emptyTile = new EmptyCubeTile();
 
     var __userCursorPosition = 0;
     var __userCursorEl = document.createElement('div');
@@ -543,7 +543,7 @@ var Playlist = function(opts) {
                 return;
             } else if (e.altKey && (keyMap[e.keyCode] === 'up'))
             {
-                playlist.insertTileAtAndMoveCursor(new Tile(cube.readSlice()));
+                playlist.insertTileAtAndMoveCursor(new CubeTile(cube.readSlice()));
                 return;
             }
 

js/removed-from-cube-for-controllers.js
@@ -654,7 +654,7 @@ Cube.prototype.listenForKeystrokes = function(opts) {
         } else if ((cube.playbackMode === 'playlist') && (cube.playlist.focus))
         {
             var tileContents = cube.getCharacterRender(char);
-            cube.playlist.insertTileAtAndMoveCursor(new Tile(tileContents))
+            cube.playlist.insertTileAtAndMoveCursor(new CubeTile(tileContents))
         }
     };
 

js/tile.js
@@ -1,122 +1,0 @@
-var Tile = function Tile(cells) {
-    try {
-        cells = JSON.parse(cells);
-    } catch (Error) {
-    }
-
-    if (!(cells instanceof Array) || (cells.length !== 64))
-    {
-        throw "Invalid cells";
-    }
-
-    var _cells = cells.slice();
-
-    this.getCells = function() {
-        return _cells.slice();
-    };
-
-    this.getAsStrips = function() {
-        var strips = [[],[],[],[],[],[],[],[]];
-        for (var i = 0; i < 8; i++)
-        {
-            for (var j = 0; j < 8; j++)
-            {
-                strips[i][j] = _cells[Math.floor(i * 8) + j];
-            }
-        }
-        return strips;
-    };
-
-    var __stripIsInvalid = function(strip) {
-        return !(strip instanceof Array) || (strip.length !== 8);
-    };
-
-    this.setFromStrips = function(strips) {
-        if (!(strips instanceof Array) ||
-            (strips.length !== 8) ||
-            strips.some(__stripIsInvalid))
-        {
-            console.error('Invalid strips provided. Cannot compose.');
-            return;
-        }
-
-        for (var i = 0; i < 8; i++)
-        {
-            for (var j = 0; j < 8; j++)
-            {
-                _cells[Math.floor(i * 8) + j] = strips[i][j];
-            }
-        }
-
-        return this;
-    };
-
-    this.reflectX = function() {
-        return this.setFromStrips(this.getAsStrips().reverse());
-    };
-
-    this.reflectY = function() {
-        var _reflectedCells = [];
-
-        for (var col = 0; col < 8; col++)
-        {
-            var reflectedRow = [];
-            for (var row = 0; row < 8; row++)
-            {
-                reflectedRow.unshift(_cells[(col * 8) + row]);
-            }
-
-            _reflectedCells = _reflectedCells.concat(reflectedRow);
-        }
-
-        _cells = _reflectedCells;
-    };
-
-    this.getPngData = function() {
-        var PNG_OUTPUT_WIDTH = 64;
-        var PNG_OUTPUT_HEIGHT = 64;
-
-        var PIXEL_MULTIPLIER_W = Math.floor(PNG_OUTPUT_WIDTH / 8);
-        var PIXEL_MULTIPLIER_H = Math.floor(PNG_OUTPUT_HEIGHT / 8);
-
-        var c = document.createElement('canvas');
-        c.width = PNG_OUTPUT_WIDTH;
-        c.height = PNG_OUTPUT_HEIGHT;
-
-        var ctx = c.getContext('2d');
-
-        var id = ctx.createImageData(1, 1);
-        var d = id.data;
-
-        for (var idx = 0, numCells = _cells.length; idx < numCells; idx++)
-        {
-            var cell = _cells[idx];
-            cell.row = !isNaN(parseInt(cell.row, 10)) ? cell.row : Math.floor(idx % 8);
-            cell.column = !isNaN(parseInt(cell.column, 10)) ? cell.column : Math.floor(idx / 8);
-
-            var pixelOffsetX = cell.row * PIXEL_MULTIPLIER_W;
-            var pixelOffsetY = cell.column * PIXEL_MULTIPLIER_H;
-
-            for (var subpixelCol = 0; subpixelCol < PIXEL_MULTIPLIER_W; subpixelCol++)
-            {
-                for (var subpixelRow = 0; subpixelRow < PIXEL_MULTIPLIER_H; subpixelRow++)
-                {
-                    d[0] = cell.color[0];
-                    d[1] = cell.color[1];
-                    d[2] = cell.color[2];
-                    d[3] = cell.on ? 255 : 0;
-
-                    var y = pixelOffsetX + subpixelRow; // the x/y are swapped in the slice serialization
-                    var x = pixelOffsetY + subpixelCol;
-
-                    ctx.putImageData(id, x, y);
-                }
-            }
-        }
-
-        return c.toDataURL();
-    };
-
-
-    return this;
-};

test-index.html
@@ -19,7 +19,8 @@
     <script src="js/cell.js"></script>
     <script src="js/cellRenderer.js"></script>
     <script src="js/cellDOMRenderer.js"></script>
-    <script src="js/tile.js"></script>
+    <script src="js/cubeTile.js"></script>
+    <script src="js/cubeAssets.js"></script>
     <script src="js/cube.js"></script>
     <script src="js/cubeRenderer.js"></script>
     <script src="js/cubeDOMRenderer.js"></script>

commit 645496dd3c5c89faee9dab9f44eb2dab1dffa3b9

fix bug introduced in 39cd2ad
These methods need to be private because they mutate the _cells array
on the Tile. Perhaps they could be rewritten later just to mutate the
state of each cell in place, but that’s a problem for another time.

js/cubeTile.js
@@ -22,64 +22,64 @@ var CubeTile = function CubeTile(cells, cellOpts) {
         get: function() { return _cells.slice(); },
     });
 
-    return this;
-};
-
-CubeTile.prototype.getAsStrips = function() {
-    var strips = [[],[],[],[],[],[],[],[]];
-    for (var i = 0; i < 8; i++)
-    {
-        for (var j = 0; j < 8; j++)
+    this.getAsStrips = function() {
+        var strips = [[],[],[],[],[],[],[],[]];
+        for (var i = 0; i < 8; i++)
         {
-            strips[i][j] = _cells[Math.floor(i * 8) + j];
+            for (var j = 0; j < 8; j++)
+            {
+                strips[i][j] = _cells[Math.floor(i * 8) + j];
+            }
         }
-    }
-    return strips;
-};
-
-CubeTile.prototype.setFromStrips = function(strips) {
-    var __stripIsInvalid = function(strip) {
-        return !(strip instanceof Array) || (strip.length !== 8);
+        return strips;
     };
 
-    if (!(strips instanceof Array) ||
-        (strips.length !== 8) ||
-        strips.some(__stripIsInvalid))
-    {
-        console.error('Invalid strips provided. Cannot compose.');
-        return;
-    }
+    this.setFromStrips = function(strips) {
+        var __stripIsInvalid = function(strip) {
+            return !(strip instanceof Array) || (strip.length !== 8);
+        };
 
-    for (var i = 0; i < 8; i++)
-    {
-        for (var j = 0; j < 8; j++)
+        if (!(strips instanceof Array) ||
+            (strips.length !== 8) ||
+            strips.some(__stripIsInvalid))
         {
-            _cells[Math.floor(i * 8) + j] = strips[i][j];
+            console.error('Invalid strips provided. Cannot compose.');
+            return;
         }
-    }
+
+        for (var i = 0; i < 8; i++)
+        {
+            for (var j = 0; j < 8; j++)
+            {
+                _cells[Math.floor(i * 8) + j] = strips[i][j];
+            }
+        }
+
+        return this;
+    };
+
+    this.reflectX = function() {
+        return this.setFromStrips(this.getAsStrips().reverse());
+    };
+
+    this.reflectY = function() {
+        var _reflectedCells = [];
+
+        for (var col = 0; col < 8; col++)
+        {
+            var reflectedRow = [];
+            for (var row = 0; row < 8; row++)
+            {
+                reflectedRow.unshift(_cells[(col * 8) + row]);
+            }
+
+            _reflectedCells = _reflectedCells.concat(reflectedRow);
+        }
+
+        _cells = _reflectedCells;
+    };
 
     return this;
-};
-
-CubeTile.prototype.reflectX = function() {
-    return this.setFromStrips(this.getAsStrips().reverse());
-};
-
-CubeTile.prototype.reflectY = function() {
-    var _reflectedCells = [];
-
-    for (var col = 0; col < 8; col++)
-    {
-        var reflectedRow = [];
-        for (var row = 0; row < 8; row++)
-        {
-            reflectedRow.unshift(_cells[(col * 8) + row]);
-        }
-
-        _reflectedCells = _reflectedCells.concat(reflectedRow);
-    }
-
-    _cells = _reflectedCells;
 };
 
 CubeTile.prototype.getPngData = function() {
@@ -98,9 +98,9 @@ CubeTile.prototype.getPngData = function() {
     var id = ctx.createImageData(1, 1);
     var d = id.data;
 
-    for (var idx = 0, numCells = _cells.length; idx < numCells; idx++)
+    for (var idx = 0, numCells = this.cells.length; idx < numCells; idx++)
     {
-        var cell = _cells[idx];
+        var cell = this.cells[idx];
         cell.row = !isNaN(parseInt(cell.row, 10)) ? cell.row : Math.floor(idx % 8);
         cell.column = !isNaN(parseInt(cell.column, 10)) ? cell.column : Math.floor(idx / 8);
 