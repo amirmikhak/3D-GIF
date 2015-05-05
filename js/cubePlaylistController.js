amirmikhak/3D-GIF

commit ec885c98c835e256dbe0c12d1b25967419328031

fix event data in propertyChanged on controllers
rename “setting” -> “property”

js/cubePlaylistController.js
@@ -646,7 +646,7 @@ var CubePlaylistController = function CubePlaylistController(opts) {
             }
 
             this.emit(PLAYLIST_SETTINGS_CHANGE_NAME, {
-                setting: 'mode',
+                property: 'mode',
                 newValue: _options['mode'],
                 oldValue: prevMode,
             });
@@ -711,7 +711,7 @@ var CubePlaylistController = function CubePlaylistController(opts) {
             }
 
             this.emit(PLAYLIST_SETTINGS_CHANGE_NAME, {
-                setting: 'writeFace',
+                property: 'writeFace',
                 newValue: _options['writeFace'],
                 oldValue: prevFace,
             });
@@ -744,7 +744,7 @@ var CubePlaylistController = function CubePlaylistController(opts) {
             }
 
             this.emit(PLAYLIST_SETTINGS_CHANGE_NAME, {
-                setting: 'wrapDirection',
+                property: 'wrapDirection',
                 newValue: _options['wrapDirection'],
                 oldValue: prevWrapDirection,
             });
@@ -758,7 +758,7 @@ var CubePlaylistController = function CubePlaylistController(opts) {
             _options['loops'] = !!shouldLoop;
 
             this.emit(PLAYLIST_SETTINGS_CHANGE_NAME, {
-                setting: 'loops',
+                property: 'loops',
                 newValue: _options['loops'],
                 oldValue: prevLoops,
             });
@@ -786,7 +786,7 @@ var CubePlaylistController = function CubePlaylistController(opts) {
             }
 
             this.emit(PLAYLIST_SETTINGS_CHANGE_NAME, {
-                setting: 'spacing',
+                property: 'spacing',
                 newValue: _options['spacing'],
                 oldValue: prevSpacing,
             });

js/cubeRealtimeUserController.js
@@ -309,7 +309,7 @@ var CubeRealtimeUserController = function CubeRealtimeUserController(opts) {
             _options['action'] = newAction;
 
             this.emit('propertyChanged', {
-                setting: 'action',
+                property: 'action',
                 newValue: _options['action'],
                 oldValue: prevAction,
             });
@@ -329,7 +329,7 @@ var CubeRealtimeUserController = function CubeRealtimeUserController(opts) {
             _options['direction'] = newDirection;
 
             this.emit('propertyChanged', {
-                setting: 'direction',
+                property: 'direction',
                 newValue: _options['direction'],
                 oldValue: prevDirection,
             });
@@ -350,7 +350,7 @@ var CubeRealtimeUserController = function CubeRealtimeUserController(opts) {
             _options['stepSize'] = newStepSize;
 
             this.emit('propertyChanged', {
-                setting: 'stepSize',
+                property: 'stepSize',
                 newValue: _options['stepSize'],
                 oldValue: prevStepSize,
             });
@@ -364,7 +364,7 @@ var CubeRealtimeUserController = function CubeRealtimeUserController(opts) {
             _options['wrap'] = !!newWrap;
 
             this.emit('propertyChanged', {
-                setting: 'wrapys',
+                property: 'wrapys',
                 newValue: _options['wrap'],
                 oldValue: prevWrap,
             });
@@ -386,7 +386,7 @@ var CubeRealtimeUserController = function CubeRealtimeUserController(opts) {
             __listenForKeystrokes();
 
             this.emit('propertyChanged', {
-                setting: 'listenForKeys',
+                property: 'listenForKeys',
                 newValue: _options['listenForKeys'],
                 oldValue: prevListenForKeys,
             });

js/cubeController.js
@@ -166,7 +166,7 @@ var CubeController = function CubeController(opts) {
             _options['penColor'] = newPenColor;
 
             this.emit('propertyChanged', {
-                setting: 'penColor',
+                property: 'penColor',
                 newValue: _options['penColor'],
                 oldValue: prevPenColor,
             });
@@ -205,7 +205,7 @@ var CubeController = function CubeController(opts) {
             _options['animationInterval'] = parsedValue;
 
             this.emit('propertyChanged', {
-                setting: 'animationInterval',
+                property: 'animationInterval',
                 newValue: _options['animationInterval'],
                 oldValue: prevAnimationInterval,
             });

commit 611a139a5810ccfb4348748c80c77aaebf9333aa

fix bugs introduced in ec885c9

js/cubePlaylistController.js
@@ -802,7 +802,7 @@ var CubePlaylistController = function CubePlaylistController(opts) {
     };
 
     this.on('propertyChanged', function(changeData) {
-        if (changeData.setting === 'animationInterval')
+        if (changeData.property === 'animationInterval')
         {
             __updateAnimationSettings();
             __updateGeneratedFrameValidTimes();

js/cubeRealtimeUserController.js
@@ -398,7 +398,7 @@ var CubeRealtimeUserController = function CubeRealtimeUserController(opts) {
     };
 
     this.on('propertyChanged', function(changeData) {
-        if (changeData.setting === 'animationInterval')
+        if (changeData.property === 'animationInterval')
         {
             if (cubeRealtimeUserController.playing)
             {